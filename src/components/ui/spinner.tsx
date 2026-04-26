35432bc9e8fe058ad9ea2f01d73045c5f486a59a
alexthecurator <zander.swai@gmail.com>
2026-04-26 14:35:21 +0300

    Fix (shadcn - prop conflict): muted @remixicon/react prop conflict on RiLoaderLine

--- src/components/ui/spinner.tsx
+++ src/components/ui/spinner.tsx
@@ -1,10 +1,15 @@
-import { cn } from "@/lib/utils"
-import { RiLoaderLine } from "@remixicon/react"
+import { cn } from "@/lib/utils";
+import { RiLoaderLine } from "@remixicon/react";
 
-function Spinner({ className, ...props }: React.ComponentProps<"svg">) {
+function Spinner({ className }: React.ComponentProps<"svg">) {
   return (
-    <RiLoaderLine role="status" aria-label="Loading" className={cn("size-4 animate-spin", className)} {...props} />
-  )
+    <RiLoaderLine
+      role="status"
+      aria-label="Loading"
+      className={cn("size-4 animate-spin", className)}
+      // {...props}
+    />
+  );
 }
 
-export { Spinner }
+export { Spinner };
